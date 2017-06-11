2969d642198bb6f183e7a63476e2883477795c46
TheBloodyScreen <JayDee@TheBloodyScreen.com>
2017-06-11 18:35:09 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+HOST = 'irc.twitch.tv' # the server you want to connect to (standart: Twitch:irc.twitch.tv)
+PORT = 6667            # the port you want to use to connect (standart: Twitch:6667)
+PASS = ''              # the pass you need to connect to chat (example: oauth:dsgfhdghslkgdlkhgshgdgshdl)
+readbuffer = ''        # a value to limit the messages per second to be processed
